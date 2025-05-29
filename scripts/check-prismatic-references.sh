5c862ac65f6b2bafad2d5fe8a35ff3285e43729d
Taylor Reece <taylor.reece@prismatic.io>
2025-05-29 16:22:42 -0500

    grep's --exclude flag behaves different on Ubuntu as on MacOS

--- scripts/check-prismatic-references.sh
+++ scripts/check-prismatic-references.sh
@@ -9,7 +9,7 @@ if [ ! -d "docs" ]; then
     exit 1
 fi
 
-if grep -i prismatic -r docs/ --exclude docs/connectors/prismatic.md; then
+if grep -i prismatic -r docs/ --exclude prismatic.md; then
     echo "Prismatic is mentioned in the docs, which is not allowed."
     echo "Please remove any references to Prismatic from the documentation."
     exit 1
